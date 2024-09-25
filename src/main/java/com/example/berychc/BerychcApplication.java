979ffefde989d4faa9a9a539a749f67beea33697
Berychc <treewincsgo@gmail.com>
2024-09-26 00:37:00 +0300

    Start Project

---
+++ src/main/java/com/example/berychc/BerychcApplication.java
@@ -0,0 +1,13 @@
+package com.example.berychc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerychcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BerychcApplication.class, args);
+	}
+
+}
